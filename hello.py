6bc852bc486d6f9b07ae5208f63886755a4e29d3
João Lima <joaovictorbernardinodelima@gmail.com>
2023-08-01 11:20:10 -0300

    Adicionamos hello world em algumas linguagens

--- hello.py
+++ hello.py
@@ -1,3 +1,34 @@
 #! /usr/bin/env python3
+"""Hello World Multi Language.
 
-print("Hello World!")
+Depending on the language configured in the environment, the program displays
+the corresponding message.
+
+How to use:
+
+Have the LANG variable properly configured ex:
+
+    export LANG=pt_BR
+
+run:
+
+    python3 hello.py
+    or
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Joao Lima"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+
+print(msg)
